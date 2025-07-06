b4f4af749af34eb71b5881d3308f5ab9bba6713b
jslament <bei.suyu.jia@gmail.com>
2025-07-06 16:42:35 +0200

    first commit

---
+++ kubejs/server_scripts/coins.js
@@ -0,0 +1,21 @@
+LootJS.modifiers((event) => {
+    // Common
+    event
+        .addTableModifier(/(minecraft:chests.*|friendsandfoes:chests.*|mowziesmobs:chests.*|ribbits:chests.*|revampedvillages:.*|skyvillages:chests.*|trek.*|)/)
+        .addLoot(LootEntry.of("magic_coins:silver_coin").setCount([3, 7]).randomChance(0.15));
+
+    // Uncommon
+    event
+        .addTableModifier(/(minecraft:chests\/(shipwreck|bastion).*|irons_spellbooks:chests.*|adventuredungeons:chests.*|takesapillage:chests.*|eternal_starlight:chests.*|artifacts:chests\/.*|ati_structures:.*|bosses_of_mass_destruction:chests.*)/)
+        .addLoot(LootEntry.of("magic_coins:silver_coin").setCount([4, 10]).randomChance(0.60));
+
+    // Rare
+    event
+        .addTableModifier(/(minecraft:chests\/(ancient_city|end_city).*|betterfortresses:chests.*|nova_structures:chests.*|dungeons_arise:chests.*|alshanex_familiars:chests\/summoner_lab\/treasure)/)
+        .addLoot(LootEntry.of("magic_coins:silver_coin").setCount([7, 12]));
+
+    // Epic
+    event
+        .addTableModifier(/cataclysm:chests\/.*/)
+        .addLoot(LootEntry.of("magic_coins:gold_coin").setCount([1, 3]));
+});
