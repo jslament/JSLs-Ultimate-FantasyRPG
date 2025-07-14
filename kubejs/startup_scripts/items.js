77755b9a3d7726a7ab7ed1d95c322ac30ffcbfc2
jslament <bei.suyu.jia@gmail.com>
2025-07-14 04:29:13 +0200

    Added relics configs. Balanced holy locket and void bubble around end game. Still need void cloak and void vortex configured

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,4 +1,8 @@
 // Listen to item registry event
 StartupEvents.registry('item', event => {
   event.create('black_pearl')
+  event.create('desert_rose')
+  event.create('pharaohs_mask')
+  event.create('wither_ribcage')
 })
+
