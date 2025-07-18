66b725fa06a8c0e047b4a1d70bb6487fb47eb0ab
jslament <bei.suyu.jia@gmail.com>
2025-07-18 20:08:07 +0200

    Added few stuff to quests. Finished first 3 eyes

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -2,7 +2,6 @@ LootJS.lootTables(event => {
     event.getLootTable("irons_spellbooks:chests/catacombs/crypt_loot").firstPool().addEntry("endrem:undead_soul")
 })
 
-// Server Script Folder -> file.js
 AStages.addRestrictionForLoot('astages/skeleton_horse_restrict', 'skeleton_horse')
     .applyForEveryLootTableAndDrop(false)
     .restrictForEntities('skeleton_horse')
--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,5 +1,10 @@
+// General Recipe Modifications
 ServerEvents.recipes(event => {
     // Might use constants for item IDs in the future but for now, using strings directly
     event.blasting('hazennstuff:deus_essence', 'minecraft:nether_star')
     event.blasting('5x hazennstuff:deus_essence', 'irons_spellbooks:divine_soulshard')
 });
+
+// Astages
+// Dimension Contorl
+AStages.addRestrictionForDimension("astages/nether_restrict", "nether", "minecraft:the_nether")
