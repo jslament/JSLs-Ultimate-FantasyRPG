0398a6237ee5390ec8d549a83fec8a2bd9f2a67d
jslament <bei.suyu.jia@gmail.com>
2025-07-20 18:03:55 +0200

    Added recipe for nether eye and anything related. Packaged some assets as my own mod. Added forbidden and arcanus

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -1,9 +1,20 @@
 LootJS.lootTables(event => {
-    event.getLootTable("irons_spellbooks:chests/catacombs/crypt_loot").firstPool().addEntry("endrem:undead_soul")
+    // In case players are unlucky and don't get the crucial items from the loot tables.
+    // Loot modifications in kubejs because I'm lazy
+    event.getLootTable("irons_spellbooks:chests/catacombs/crypt_loot").firstPool().addEntry(LootEntry.of("endrem:undead_soul").withWeight(5))
+    event.getLootTable("irons_spellbooks:chests/citadel/citadel_vault").firstPool().addEntry("kubejs:molten_tablet")
+    event.getLootTable("irons_spellbooks:entities/fire_boss").firstPool().addEntry("kubejs:molten_tablet")
 })
+
 
 AStages.addRestrictionForLoot('astages/skeleton_horse_restrict', 'skeleton_horse')
     .applyForEveryLootTableAndDrop(false)
     .restrictForEntities('skeleton_horse')
     .restrictItems('endrem:undead_soul')
-    .setEntityFilter('partial');
+    .setEntityFilter('partial')
+
+AStages.addRestrictionForLoot('astages/citadel_keeper_restrict', 'citadel_keeper')
+    .applyForEveryLootTableAndDrop(false)
+    .restrictForEntities('irons_spellbooks:citadel_keeper')
+    .restrictItems('kubejs:molten_tablet')
+    .setEntityFilter('partial')
--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -3,6 +3,18 @@ ServerEvents.recipes(event => {
     // Might use constants for item IDs in the future but for now, using strings directly
     event.blasting('hazennstuff:deus_essence', 'minecraft:nether_star')
     event.blasting('5x hazennstuff:deus_essence', 'irons_spellbooks:divine_soulshard')
+    event.shaped(
+        Item.of('kubejs:wildfire_essence', 1),
+        [
+         ' B ',
+         'BAB',
+         ' B '
+        ],
+        {
+        A: 'hazennstuff:deus_essence',
+        B: 'friendsandfoes:wildfire_crown_fragment'
+        }
+)
 });
 
 // Astages
