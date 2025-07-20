658a9463fbe4151a57c1d9f986d926e8c10e9aa8
jslament <bei.suyu.jia@gmail.com>
2025-07-20 22:33:45 +0200

    Corrupted eye recipe. Still need to do related content. Very few designs

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -1,18 +1,19 @@
-LootJS.lootTables(event => {
     // In case players are unlucky and don't get the crucial items from the loot tables.
     // Loot modifications in kubejs because I'm lazy
+LootJS.lootTables(event => {
     event.getLootTable("irons_spellbooks:chests/catacombs/crypt_loot").firstPool().addEntry(LootEntry.of("endrem:undead_soul").withWeight(5))
     event.getLootTable("irons_spellbooks:chests/citadel/citadel_vault").firstPool().addEntry("kubejs:molten_tablet")
     event.getLootTable("irons_spellbooks:entities/fire_boss").firstPool().addEntry("kubejs:molten_tablet")
 })
 
-
+    // Undead Eye
 AStages.addRestrictionForLoot('astages/skeleton_horse_restrict', 'skeleton_horse')
     .applyForEveryLootTableAndDrop(false)
     .restrictForEntities('skeleton_horse')
     .restrictItems('endrem:undead_soul')
     .setEntityFilter('partial')
 
+    // Nether Eye
 AStages.addRestrictionForLoot('astages/citadel_keeper_restrict', 'citadel_keeper')
     .applyForEveryLootTableAndDrop(false)
     .restrictForEntities('irons_spellbooks:citadel_keeper')
