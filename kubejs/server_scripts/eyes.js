0e526754db4ac8b38168c755e86712ca572651f6
jslament <bei.suyu.jia@gmail.com>
2025-07-21 18:33:46 +0200

    Added recipe for starlit eye and related. Added gambling

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -1,10 +1,14 @@
     // In case players are unlucky and don't get the crucial items from the loot tables.
     // Loot modifications in kubejs because I'm lazy
 LootJS.lootTables(event => {
-    event.getLootTable("irons_spellbooks:chests/catacombs/crypt_loot").firstPool().addEntry(LootEntry.of("endrem:undead_soul").withWeight(5))
-    event.getLootTable("irons_spellbooks:chests/citadel/citadel_vault").firstPool().addEntry("kubejs:molten_tablet")
-    event.getLootTable("irons_spellbooks:entities/fire_boss").firstPool().addEntry("kubejs:molten_tablet")
-    event.getLootTable("mowziesmobs:entities/umvuthi").firstPool().addEntry(LootEntry.of("kubejs:tribal_totem").withWeight(2))
+    // Undead Eye
+    event.getLootTable('irons_spellbooks:chests/catacombs/crypt_loot').firstPool().addEntry(LootEntry.of('endrem:undead_soul').withWeight(5))
+
+    //
+    event.getLootTable('irons_spellbooks:chests/citadel/citadel_vault').firstPool().addEntry('kubejs:molten_tablet')
+    event.getLootTable('irons_spellbooks:entities/fire_boss').firstPool().addEntry('kubejs:molten_tablet')
+    event.getLootTable('mowziesmobs:entities/umvuthi').firstPool().addEntry(LootEntry.of('kubejs:tribal_totem').withWeight(2))
+    event.getLootTable('eternal_starlight:entities/lonestar_skeleton').firstPool().addEntry(LootEntry.of('forbidden_arcanus:crescent_moon').matchMainHand('eternal_starlight:moonring_greatsword'))
 })
 
     // Undead Eye
@@ -20,5 +24,3 @@ AStages.addRestrictionForLoot('astages/citadel_keeper_restrict', 'citadel_keeper
     .restrictForEntities('irons_spellbooks:citadel_keeper')
     .restrictItems('kubejs:molten_tablet')
     .setEntityFilter('partial')
-
-    // Corrupted Eye
--- kubejs/server_scripts/recipe_removals.js
+++ kubejs/server_scripts/recipe_removals.js
@@ -1,4 +1,6 @@
 ServerEvents.recipes(event => {
     // Might use constants for item IDs in the future but for now, using strings directly
     event.remove({ output: 'hazennstuff:deus_essence' })
-});
+    event.remove({ output: 'sophisticatedbackpacks:stack_upgrade_omega_tier' })
+    event.remove({ output: 'sophisticatedstorage:stack_upgrade_omega_tier' })
+})
