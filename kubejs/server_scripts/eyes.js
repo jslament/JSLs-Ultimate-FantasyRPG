8a37c138fe3c134d670edaecfa69502b551645c0
jslament <bei.suyu.jia@gmail.com>
2025-07-22 17:24:23 +0200

    Quest descriptions for more eyes. More Astages gating and few misc changes

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -1,13 +1,16 @@
-    // In case players are unlucky and don't get the crucial items from the loot tables.
     // Loot modifications in kubejs because I'm lazy
 LootJS.lootTables(event => {
     // Undead Eye
     event.getLootTable('irons_spellbooks:chests/catacombs/crypt_loot').firstPool().addEntry(LootEntry.of('endrem:undead_soul').withWeight(5))
 
-    //
+    // Nether Eye
     event.getLootTable('irons_spellbooks:chests/citadel/citadel_vault').firstPool().addEntry('kubejs:molten_tablet')
     event.getLootTable('irons_spellbooks:entities/fire_boss').firstPool().addEntry('kubejs:molten_tablet')
+
+    // Corrupted Eye
     event.getLootTable('mowziesmobs:entities/umvuthi').firstPool().addEntry(LootEntry.of('kubejs:tribal_totem').withWeight(2))
+
+    // Starlit Eye
     event.getLootTable('eternal_starlight:entities/lonestar_skeleton').firstPool().addEntry(LootEntry.of('forbidden_arcanus:crescent_moon').matchMainHand('eternal_starlight:moonring_greatsword'))
 })
 
--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -3,6 +3,7 @@ ServerEvents.recipes(event => {
     // Might use constants for item IDs in the future but for now, using strings directly
     event.blasting('hazennstuff:deus_essence', 'minecraft:nether_star')
     event.blasting('5x hazennstuff:deus_essence', 'irons_spellbooks:divine_soulshard')
+    
     event.shaped(
         Item.of('kubejs:wildfire_essence', 1),
         [
@@ -14,9 +15,10 @@ ServerEvents.recipes(event => {
         A: 'hazennstuff:deus_essence',
         B: 'friendsandfoes:wildfire_crown_fragment'
         }
-)
+    )
 });
 
 // Astages
 // Dimension Contorl
 AStages.addRestrictionForDimension("astages/nether_restrict", "nether", "minecraft:the_nether")
+AStages.addRestrictionForDimension("astages/starlight_restrict", "starlight", "eternal_starlight:starlight")
