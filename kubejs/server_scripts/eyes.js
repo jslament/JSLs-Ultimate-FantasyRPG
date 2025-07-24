b4ee4d62c018eba8f387421556447f14a07d4103
jslament <bei.suyu.jia@gmail.com>
2025-07-24 03:42:36 +0200

    Added tons of recipes and loot tables for progression.

--- kubejs/server_scripts/eyes.js
+++ kubejs/server_scripts/eyes.js
@@ -1,6 +1,10 @@
 LootJS.lootTables(event => {
     // Starlit Eye
     event.getLootTable('eternal_starlight:entities/lonestar_skeleton').firstPool().addEntry(LootEntry.of('forbidden_arcanus:crescent_moon').matchMainHand('eternal_starlight:moonring_greatsword'))
+
+    // Cold Eye
+    event.getLootTable('mowziesmobs:entities/frostmaw').firstPool().addEntry(LootEntry.of('endrem:cold_eye').matchMainHand('hazennstuff:skyscorcher'))
+    
 })
 
     // Undead Eye
